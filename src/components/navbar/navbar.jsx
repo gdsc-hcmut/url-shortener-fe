ec5e0768255586c257d6332ee1231ccf151201cf
jamesklein218 <jamesklein218@gmail.com>
2022-01-11 20:03:57 +0700

    [f] 456A Make use of view width (vw)
    - Change parameter to view-width
    - Change file name from navbarlink to navbarbutton
    
    [#180834279]  https://www.pivotaltracker.com/story/show/180834279

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
+import NavBarButton from './navbarbutton';
 import NavBarHome from './navbarhome';
-import NavBarLink from './navbarlink';
 
 export default function NavBar() {
   return (
-    <nav className="shadow-lg max-w-full h-fit w-full flex px-[60px] py-[30px]">
-      <div className="flex h-full w-full justify-between">
+    <nav className="shadow-lg max-w-full h-fit w-full flex px-[3vw] py-[1.5vw]">
+      <div className="flex h-full w-full justify-between align-center">
         <NavBarHome />
-        <NavBarLink />
+        <NavBarButton />
       </div>
     </nav>
   );
--- src/components/navbar/navbarhome.jsx
+++ src/components/navbar/navbarhome.jsx
@@ -5,8 +5,8 @@ import Logo from 'assets/image/logo.svg';
 export default function NavBarHome() {
   return (
     <img
+      className="w-[401.05px] h-[50px] mr-[30px]"
       src={Logo}
-      style={{ height: 50, width: 407.05 }}
       alt="website logo"
     />
   );
