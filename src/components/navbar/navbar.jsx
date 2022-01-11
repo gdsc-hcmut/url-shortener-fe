ede3f3b1f4aafbf1eba84d14fb65b4dc06752293
jamesklein218 <jamesklein218@gmail.com>
2022-01-11 15:22:22 +0700

    [f] 456A Acquire latest changes on initialization branch
    - Use .jsx and appropriate import for .jsx files
    
    [#180834279]  https://www.pivotaltracker.com/story/show/180834279

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import NavBarHome from './navbarhome';
 import NavBarLink from './navbarlink';
 
--- src/components/navbar/navbarhome.jsx
+++ src/components/navbar/navbarhome.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import Logo from 'assets/image/logo.svg';
 
 export default function NavBarHome() {
