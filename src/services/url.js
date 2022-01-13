e1a1271211aec0d14b609a2504b8a945d9365915
Viet Duc Nguyen <duc.nv291@gmail.com>
2022-01-13 20:54:03 +0700

    [c] - Use API_URL environment variable

--- src/services/url.js
+++ src/services/url.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-import API_URL from 'config';
+// import API_URL from 'config';
+
+const API_URL = process.env;
 
 const UrlAPI = {
   shortenUrl: (longUrl) => axios.post(`${API_URL}/api/url/shorten`, { longUrl }),
