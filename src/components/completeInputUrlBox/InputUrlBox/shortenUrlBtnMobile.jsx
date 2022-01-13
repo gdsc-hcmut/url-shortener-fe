98f186a9c93bed4dd17704cc307e4aaf7cda5a1f
Viet Duc Nguyen <duc.nv291@gmail.com>
2022-01-13 11:28:37 +0700

    [f] - Delete folder

--- src/components/completeInputUrlBox/InputUrlBox/shortenUrlBtnMobile.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function ShortenUrlBtnMobile() {
-  return (
-    <div>
-      <button type="button" className="text-[0.938rem] text-white md:hidden w-[9.735rem] h-[3.75rem] bg-royal-blue rounded-lg hover:bg-shorten-btn-hover">Shorten</button>
-    </div>
-  );
-}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
 import UrlInputBoxAndTitle from 'components/CompleteInputUrlBox/UrlInputBoxAndTitle';
+import Footer from 'components/Footer';
+import NavBar from 'components/Navbar';
 import Url from 'components/Url';
 
-export default function HomePage() {
+export default function Homepage() {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center items-center ">
+      <NavBar />
       <h1 className="text-3xl text-blue-600 underline">My URL</h1>
       <UrlInputBoxAndTitle />
       <Url />
+      <Footer />
     </div>
   );
 }
