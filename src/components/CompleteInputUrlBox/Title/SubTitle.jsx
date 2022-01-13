d54fe5da162d3dac594cd22f84dfba8bc19ca11c
Viet Duc Nguyen <duc.nv291@gmail.com>
2022-01-13 11:57:31 +0700

    [f] 456 - Create UI for Homepage
    - Create header for mobile version
    
    [#180848456] https://www.pivotaltracker.com/story/show/180848456

--- src/components/CompleteInputUrlBox/Title/SubTitle.jsx
+++ src/components/CompleteInputUrlBox/Title/SubTitle.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 export default function SubTitle() {
   return (
-    <div className="md:mt-[2vh] flex flex-col md:items-center flex-end h-[1.527vh] space-y-[6px] md:flex-row md:space-x-[0.46875vw] md:space-y-0">
+    <div className="mb-[23px] md:mb-0 md:mt-[2vh] flex flex-col md:items-center flex-end h-[1.527vh] space-y-[6px] md:flex-row md:space-x-[0.46875vw] md:space-y-0">
       <p className="text-[0.75rem] font-normal text-dim-gray">
         Want more details on your previous shortened link?
       </p>
       <button
         type="submit"
-        className="md:m-0 w-[4.453rem] h-[1.2rem] b-[50px] bg-get-started-btn rounded-[60px] flex justify-center items-center hover:bg-get-started-btn-hover"
+        className="md:m-0 w-[5.625rem] h-[1.563rem] b-[50px] bg-get-started-btn rounded-[60px] flex justify-center items-center hover:bg-get-started-btn-hover"
       >
         <div className="opacity-100 text-[0.643rem] font-normal text-royal-blue flex justify-center items-center">
           Get started
--- src/components/Navbar/NavbarButton.jsx
+++ src/components/Navbar/NavbarButton.jsx
@@ -6,8 +6,8 @@ export default function NavBarButton() {
       type="button"
       className="bg-blue-100 ease-out duration-300 hover:bg-blue-200 bg-opacity-10
                 text-gdscBlue font-normal hover:bg-opacity-10
-                rounded w-auto h-auto content-center text-[15px]
-                py-[15px] px-[3.7vw] ml-[30px]"
+                rounded w-[24.62vw] md:w-auto h-auto content-center text-[15px]
+                py-[15px] my-[22px] md:my-0 px-[3.7vw] ml-[30px]"
     >
       <p>Login</p>
     </button>
