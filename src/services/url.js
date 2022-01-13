ff3777e758a19198794b94655a8b68b87e0f1683
Viet Duc Nguyen <duc.nv291@gmail.com>
2022-01-13 21:12:54 +0700

    [c] - Fix typo

--- src/services/url.js
+++ src/services/url.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 // import API_URL from 'config';
 
-const API_URL = process.env;
+const { REACT_APP_API_URL } = process.env;
 
 const UrlAPI = {
-  shortenUrl: (longUrl) => axios.post(`${API_URL}/api/url/shorten`, { longUrl }),
+  shortenUrl: (longUrl) => axios.post(`${REACT_APP_API_URL}/api/url/shorten`, { longUrl }),
 };
 
 export default UrlAPI;
