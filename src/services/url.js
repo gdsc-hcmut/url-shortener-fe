96a849663acfa3cba09a384fd59217483b8ac74c
Viet Duc Nguyen <duc.nv291@gmail.com>
2022-01-15 11:49:42 +0700

    [c] 456C - Change service url

--- src/services/url.js
+++ src/services/url.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const { REACT_APP_API_URL } = process.env;
 
 const UrlAPI = {
-  shortenUrl: (longUrl) => axios.post(`${REACT_APP_API_URL}/api/url/shorten`, { longUrl }),
+  shortenUrl: (longUrl) => axios.post(`${REACT_APP_API_URL}/api/urls`, { longUrl }),
 };
 
 export default UrlAPI;
