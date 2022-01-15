114b639609d7837d8ba7224964b6dfe1261c768c
Trần Duy Minh <minhduytranct2017@gmail.com>
2022-01-15 22:41:56 +0700

    [f] 456 - Fix size of title and input Url field
    - Change size from vw to px based on figma
    - Switch to tailwind config in some figure
    
    [#180848456] https://www.pivotaltracker.com/story/show/180848456

--- src/components/UrlInputBoxAndTitle/HomepageText/SubTitle.jsx
+++ src/components/UrlInputBoxAndTitle/HomepageText/SubTitle.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 export default function SubTitle() {
   return (
-    <div className="mb-[23px] md:mb-0 md:mt-[2vh] flex flex-col md:items-center flex-end h-[1.527vh] space-y-[6px] md:flex-row md:space-x-[0.46875vw] md:space-y-0">
-      <p className="text-[0.75rem] font-normal text-gdscGrey-700">
+    <div className="md:mb-0 md:mt-3.5 flex flex-col md:items-center flex-end md:h-6 space-y-1.5 md:flex-row md:space-x-3.5 md:space-y-0">
+      <p className="text-base font-normal text-gdscGrey-700">
         Want more details on your previous shortened link?
       </p>
       <button
         type="submit"
-        className="md:m-0 w-[5.625rem] h-[1.563rem] b-[50px] bg-get-started-btn rounded-[60px] flex justify-center items-center hover:bg-get-started-btn-hover  ease-out duration-300"
+        className="md:m-0 w-[92px] h-6 b-[50px] bg-get-started-btn rounded-[60px] flex justify-center items-center hover:bg-get-started-btn-hover  ease-out duration-300"
       >
-        <div className="opacity-100 text-[0.643rem] font-normal text-gdscBlue-300 flex justify-center items-center">
+        <div className="opacity-100 text-xs font-normal text-gdscBlue-300 flex justify-center items-center">
           Get started
         </div>
       </button>
--- src/components/UrlInputBoxAndTitle/InputUrlField/index.jsx
+++ src/components/UrlInputBoxAndTitle/InputUrlField/index.jsx
@@ -12,48 +12,43 @@ export default function InputUrlField() {
   const handleClick = () => dispatch(shortenUrl(longUrl));
 
   return (
-    <div className="flex flex-col md:flex-row space-y-5 md:space-y-0 rounded-[0.5rem]">
-      <div className="relative hidden md:block bg-white md:w-[46.25rem] md:h-[6.25rem] rounded-[0.5rem] border shadow-lg border-gdscGrey-200">
-        <div className="w-[16.25rem] h-12 absolute left-[1.938rem] top-[1.563rem] flex-col space-y-2">
+    <div className="flex flex-col md:flex-row space-y-3 md:space-y-0 rounded-[8px]">
+      <div className="relative hidden md:block bg-white md:w-[740px] md:h-[100px] rounded-[8px] border shadow-lg border-gdscGrey-200">
+        <div className="w-[260px] h-12 absolute left-8 top-6 flex-col space-y-2">
           <p className="text-base font-medium h-5">
             <strong>Your URL</strong>
           </p>
           <input
             value={longUrl}
             onChange={handleLongUrl}
-            className="text-[0.938rem] font-normal text-gdscGrey-700 h-5 w-[16.25rem] border-b-1 outline-none "
+            className="text-base font-normal text-gdscGrey-700 h-5 w-[16.25rem] border-b-1 outline-none "
             placeholder="Input the URL you want to shorten"
           />
         </div>
-        <ReactLogo className="absolute top-[3.188rem] left-[18.25rem]" />
+        <ReactLogo className="absolute top-12 left-[292px]" />
         <div>
           <button
             type="button"
-            className="absolute inset-y-5 right-[21px] hidden text-[0.938rem] text-white md:block w-[9.735rem] h-[3.75rem] bg-gdscBlue-300 rounded-[8px] hover:bg-shorten-btn-hover ease-out duration-300 "
+            className="absolute inset-y-5 right-5 hidden text-base text-white md:block w-[152px] h-[60px] bg-gdscBlue-300 rounded-[8px] hover:bg-shorten-btn-hover ease-out duration-300 "
             onClick={handleClick}
           >
             Shorten
           </button>
         </div>
       </div>
-      <div className="relative md:hidden bg-white rounded-[0.5rem] mr-[1.875rem] h-[4.375rem] flex items-center pl-[1.875rem] space-x-[21px] rounded-md border shadow-lg border-gdscGrey-200">
+      <div className="relative md:hidden bg-white rounded-[8px] mr-5 h-[70px] flex items-center pl-5 space-x-5 rounded-md border shadow-lg border-gdscGrey-200">
         <ReactLogo />
         <input
-          className="text-[0.938rem] font-normal text-gdscGrey-700 h-5 w-[100%] bg-white"
+          value={longUrl}
+          onChange={handleLongUrl}
+          className="text-base font-normal text-gdscGrey-700 h-5 w-full bg-white"
           placeholder="Input the URL you want to shorten"
         />
-        <div>
-          <button
-            type="button"
-            className="absolute inset-y-5 right-[21px] hidden text-[0.938rem] text-white md:block w-[9.735rem] h-[3.75rem] bg-gdscBlue-300 rounded hover:bg-shorten-btn-hover ease-out duration-300 "
-          >
-            Shorten
-          </button>
-        </div>
       </div>
       <button
         type="button"
-        className="text-[0.938rem] text-white md:hidden w-[9.735rem] h-[3.75rem] bg-gdscBlue-300 rounded hover:bg-shorten-btn-hover"
+        className="text-base text-white md:hidden w-[152px] h-[60px] bg-gdscBlue-300 rounded hover:bg-shorten-btn-hover"
+        onClick={handleClick}
       >
         Shorten
       </button>
