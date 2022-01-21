c06ded6ad20484850d6c8262761e6565dd46a14a
Trần Duy Minh <minhduytranct2017@gmail.com>
2022-01-21 10:38:03 +0700

    [c] 959 - Sovle modal pop up before the url is shortened
    - Remove mistakes in using async
    - Modal will show "Shortening..." until the url is ready
    
    [#180924959] https://www.pivotaltracker.com/story/show/180924959

--- src/components/Url.jsx
+++ src/components/Url.jsx
@@ -2,6 +2,18 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 export default function Url({ shortenedUrl }) {
+  if (shortenedUrl == null) {
+    return (
+      <a
+        href={shortenedUrl}
+        target="_blank"
+        rel="noreferrer"
+        className="text-gdscGrey-800 py-[12px] text-base whitespace-nowrap md:text-base ml-[20px] md:ml-[8px] mr-[20px] w-[240px] overflow-auto no-scrollbar"
+      >
+        Shortening...
+      </a>
+    );
+  }
   return (
     <a
       href={shortenedUrl}
