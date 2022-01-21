424eb9063dfa68a2c160e1852b6f7d026030cba2
Viet Duc Nguyen <duc.nv291@gmail.com>
2022-01-21 15:47:55 +0700

    [f] 445 - Add sign up/in pages
    - Add sign up page.
    - Add sign in page.
    - Connect to Firebase.
    
    [#180846445] https://www.pivotaltracker.com/story/show/180846445

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import LoginPage from './pages/LoginPage';
 import ModalPage from './pages/ModalPage';
+import SignInPage from './pages/SignInPage';
+import SignUpPage from './pages/SignUpPage';
 import store from './store';
 import './index.css';
 
@@ -16,6 +18,8 @@ export default function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="/modal" element={<ModalPage />} />
           <Route path="/log-in" element={<LoginPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/sign-in" element={<SignInPage />} />
         </Routes>
       </BrowserRouter>
     </Provider>
