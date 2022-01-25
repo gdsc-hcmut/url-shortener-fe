73fa4ca15953197dc9301438ec55240b1d968170
Viet Duc Nguyen <duc.nv291@gmail.com>
2022-01-25 09:21:26 +0700

    [f] 445 - Setup axios interceptor & create redux actions
    - Setup axios interceptor
    - Add auth actions: loadUser, register, login, logout.
    - Extract logic into services: register, login, logout, getCurrentUser.
    - Create token services.
    
    [#180846445] https://www.pivotaltracker.com/story/show/180846445

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import { loadUser } from 'actions/auth';
 import ResetPasswordPage from 'pages/ResetPasswordPage';
 import { loadUser } from 'actions/auth';
 import HomepageLogin from 'pages/HomepageLogin';
