c42ac0d46c6ccf7a86ad7c99fc581539d5e8105f
Trần Duy Minh <minhduytranct2017@gmail.com>
2022-01-27 00:04:05 +0700

    [f] 414 - Fix error: have to click login two times to open modal
    - Fix mistake in the order of prop of showModal reducer
    - Move modal disatch into a seperate file in the action folder
    
    [#181003414] https://www.pivotaltracker.com/story/show/181003414

--- src/components/Navbar/NavbarButton.jsx
+++ src/components/Navbar/NavbarButton.jsx
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { SHOW_MODAL } from 'action-types';
