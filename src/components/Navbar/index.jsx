fc41ff9ea447c213cf53d890b54d9174e2b1bf35
Trần Duy Minh <minhduytranct2017@gmail.com>
2022-01-26 09:55:46 +0700

    [f] 412 - Create SignUp Page for mobile
    [#181003412] https://www.pivotaltracker.com/story/show/181003412

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,15 +1,19 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
 import NavbarButton from './NavbarButton';
 import NavbarHome from './NavbarHome';
 
-export default function Navbar() {
+export default function Navbar({ isModalPage }) {
   return (
     <nav className="shadow-lg max-w-full h-fit w-full flex px-[20px] py-[20px] md:px-[60px] md:py-[30px] bg-white">
       <div className="flex h-full w-full justify-between align-center">
         <NavbarHome />
-        <NavbarButton />
+        {!isModalPage ? <NavbarButton /> : <div> </div>}
       </div>
     </nav>
   );
 }
+Navbar.propTypes = {
+  isModalPage: PropTypes.bool.isRequired,
+};
--- src/pages/SignUpDesktop.jsx
+++ src/pages/SignUpDesktop.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
 
-import SignUpDesktopn from 'components/SignUpDesktop';
+import SignUpDesktopn from 'components/SignUpModal';
+import SignUpModalMobile from 'components/SignUpModal/SignUpModalMobile';
 
 export default function App() {
   const [show, setShow] = useState(false);
 
   return (
     <div>
-      <button type="button" onClick={() => setShow(true)}>
+      <button
+        type="button"
+        onClick={() => setShow(true)}
+        className="hidden md:block"
+      >
         Get started
       </button>
       <SignUpDesktopn
@@ -15,6 +20,9 @@ export default function App() {
         onClose={() => setShow(false)}
         show={show}
       />
+      <div className="md:hidden">
+        <SignUpModalMobile />
+      </div>
     </div>
   );
 }
