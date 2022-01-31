bc4b2993cf282b158d13925648d98715342e9d7f
Trần Duy Minh <minhduytranct2017@gmail.com>
2022-01-31 19:53:41 +0700

    [f] 384 - Add navbar modal to change password page
    [#181024384] https://www.pivotaltracker.com/story/show/181024384

--- src/pages/ChangePasswordPage.jsx
+++ src/pages/ChangePasswordPage.jsx
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from 'react';
 
 import ChangePassword from 'components/ChangePassword';
 import Footer from 'components/Footer';
+import NavbarModal from 'components/Modals/NavbarModal';
 import NavbarLogin from 'components/Navbar/NavbarLogin';
 
 export default function ChangePasswordPage() {
   const [width, setWidth] = useState(window.innerWidth);
-
+  const [show, setShow] = useState(false);
   function handleWindowSizeChange() {
     setWidth(window.innerWidth);
   }
@@ -18,9 +19,33 @@ export default function ChangePasswordPage() {
   }, []);
 
   const isMobile = width <= 768;
+  const showModal = () => setShow(true);
+  const hideModal = () => setShow(false);
+  const showNavbarModal = (e) => {
+    e.stopPropagation();
+    showModal();
+  };
+  const closeOnEscapeKeyDown = (e) => {
+    if ((e.charCode || e.keyCode) === 27) {
+      hideModal();
+    }
+  };
+
+  useEffect(() => {
+    document.body.addEventListener('keydown', closeOnEscapeKeyDown);
+    return function cleanup() {
+      document.body.removeEventListener('keydown', closeOnEscapeKeyDown);
+    };
+  }, []);
   return (
-    <div className="flex flex-col justify-center items-center md:items-center bg-white md:bg-gdscGrey-100">
-      <NavbarLogin />
+    <div
+      aria-hidden="true"
+      className="flex flex-col justify-center items-center md:items-center bg-white md:bg-gdscGrey-100"
+      onClick={hideModal}
+      onKeyDown={closeOnEscapeKeyDown}
+    >
+      <NavbarLogin showModal={showNavbarModal} myUrl={false} />
+      <NavbarModal show={show} />
       <div className="mt-10 mb-[120px] md:mt-16 md:self-start md:mb-[22.428vh] md:w-full">
         <ChangePassword />
       </div>
