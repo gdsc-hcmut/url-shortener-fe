f14711ad10533ab9db0702e5b0b149e4a3e57224
Trần Duy Minh <minhduytranct2017@gmail.com>
2022-02-07 22:49:00 +0700

    [f] 384 - Fix error in import order of App.jsx
    [#181024384] https://www.pivotaltracker.com/story/show/181024384

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import ChangePasswordPage from 'pages/ChangePasswordPage';
 import { loadUser } from 'actions/auth';
 import HomepageLogin from 'pages/HomepageLogin';
 import ResetPasswordPage from 'pages/ResetPasswordPage';
@@ -15,7 +14,6 @@ import SignInPage from './pages/SignInPage';
 import SignUpDesktop from './pages/SignUpDesktop';
 import SignUpPage from './pages/SignUpPage';
 import store from './store';
-
 import './index.css';
 import setAuthToken from './utils/setAuthToken';
 
