2f7728dbb97a45fa72b0cf49b3bb222b86cd8541
Trần Duy Minh <minhduytranct2017@gmail.com>
2022-02-08 10:58:45 +0700

    [f] 442C - Change styles of date time picker
    - Add edit button to open picker
    - Adjust expire time display
    
    [#181165064] https://www.pivotaltracker.com/story/show/181165064

--- src/pages/DateTimePicker.jsx
+++ src/pages/DateTimePicker.jsx
@@ -7,14 +7,29 @@ import * as React from 'react';
 
 export default function MaterialUIPickers() {
   const [value, setValue] = React.useState(new Date());
+  const [open, setOpen] = React.useState(false);
+  const weekday = new Array(7);
+  weekday[1] = 'Monday';
+  weekday[2] = 'Tuesday';
+  weekday[3] = 'Wednesday';
+  weekday[4] = 'Thursday';
+  weekday[5] = 'Friday';
+  weekday[6] = 'Saturday';
+  weekday[0] = 'Sunday';
   return (
     <div className="mt-2">
-      <div className="mx-2 text-gdscRed-200">
-        {value.toDateString()}
-        {' '}
+      <div className="mx-2 text-gdscRed-300">
+        {weekday[value.getDay()]}
+        , (
+        {value.getDate()}
+        /
+        {value.getMonth()}
+        /
+        {value.getFullYear()}
+        )
         {value.toLocaleTimeString()}
       </div>
-      <div className="bg-white text-gdscRed-300">
+      <div className="opacity-0">
         <LocalizationProvider dateAdapter={AdapterDateFns}>
           <MobileDateTimePicker
             value={value}
@@ -22,6 +37,9 @@ export default function MaterialUIPickers() {
             onChange={(newValue) => {
               setValue(newValue);
             }}
+            onAccept={() => setOpen(false)}
+            onClose={() => setOpen(false)}
+            open={open}
             minDate={new Date('2018-01-01T00:00')}
             inputFormat="yyyy/MM/dd hh:mm a"
             mask="___/__/__ __:__ _M"
@@ -31,6 +49,13 @@ export default function MaterialUIPickers() {
           />
         </LocalizationProvider>
       </div>
+      <button
+        type="button"
+        onClick={() => setOpen(true)}
+        className="text-gdscRed-300 w-[100px] h-7 bg-gdscRed-100 rounded-[60px] bg-opacity-50"
+      >
+        Edit
+      </button>
     </div>
   );
 }
