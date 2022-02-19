2601248a2b487e79db22832d36c5befb8a59f57a
Viet Duc Nguyen <duc.nv291@gmail.com>
2022-02-19 21:18:35 +0700

    [b] - Fix error

--- src/App.jsx
+++ src/App.jsx
@@ -22,10 +22,7 @@ import SignInPage from 'pages/SignInPage';
 import SignUpDesktop from 'pages/SignUpDesktop';
 import SignUpPage from 'pages/SignUpPage';
 import SnackbarPage from 'pages/SnackbarPage';
-<<<<<<< HEAD
 import StatisticPage from 'pages/StatisticPage';
-=======
->>>>>>> [f] 465 - Fix bugs
 import UrlWithSlug from 'pages/UrlWithSlug';
 import UserProfilePage from 'pages/UserProfilePage';
 
