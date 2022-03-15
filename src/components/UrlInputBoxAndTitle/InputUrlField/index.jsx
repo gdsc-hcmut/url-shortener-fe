15594f0186f7bfd2d6890c1fe0c19d285fcd7ff7
jamesklein218 <jamesklein218@gmail.com>
2022-03-15 22:37:05 +0700

    [f] 992 - Add yup to input url
    [#181311992] https://www.pivotaltracker.com/story/show/181311992

--- src/components/UrlInputBoxAndTitle/InputUrlField/index.jsx
+++ src/components/UrlInputBoxAndTitle/InputUrlField/index.jsx
@@ -1,29 +1,43 @@
-import React, { useState } from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
+import React, { useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
 import { useDispatch, useStore } from 'react-redux';
+import * as yup from 'yup';
 
 import { SHOW_URL_MODAL } from 'action-types';
 import urlAction from 'actions/url';
 import loadingIcon from 'assets/icons/loading.svg';
 import { ReactComponent as ReactLogo } from 'assets/image/web.svg';
 
+const schema = yup
+  .object({
+    longUrlDesktop: yup.string().url('vkkdvk').required('Url required'),
+    longUrlMobile: yup.string(),
+  })
+  .required();
+
 export default function InputUrlField() {
-  const [longUrl, setLongUrl] = useState('');
-  const [alert, setAlert] = useState(false);
   const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
   const store = useStore();
-  const handleLongUrl = (e) => setLongUrl(e.target.value);
-  const handleClick = async (e) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const handleClickDesktop = async (data, e) => {
     e.preventDefault();
-
-    if (longUrl) {
+    const { longUrlDesktop } = data;
+    console.log(data);
+    if (longUrlDesktop) {
       setLoading(true);
-      await dispatch(urlAction.shortenUrl(longUrl));
+      await dispatch(urlAction.shortenUrl(longUrlDesktop));
       const reduxState = store.getState();
       if (reduxState.url.error.msg === 'Bad Request') {
         setLoading(reduxState.url.loading);
-        setAlert(true);
-        setTimeout(() => setAlert(false), 3000);
       } else {
         dispatch({
           type: SHOW_URL_MODAL,
@@ -33,11 +47,33 @@ export default function InputUrlField() {
       }
     }
   };
+  const handleClickMobile = async (data, e) => {
+    e.preventDefault();
+    const { longUrlMobile } = data;
+    console.log(data);
+    if (longUrlMobile) {
+      setLoading(true);
+      await dispatch(urlAction.shortenUrl(longUrlMobile));
+      const reduxState = store.getState();
+      if (reduxState.url.error.msg === 'Bad Request') {
+        setLoading(reduxState.url.loading);
+      } else {
+        dispatch({
+          type: SHOW_URL_MODAL,
+          payload: true,
+        });
+        setLoading(reduxState.url.loading);
+      }
+    }
+  };
+  useEffect(() => {
+    reset();
+  }, []);
   return (
     <div className="flex flex-col md:flex-row space-y-5 md:space-y-0 rounded-[8px]">
       <form
         className="relative hidden md:block bg-white md:w-[740px] md:h-[100px] rounded-[8px] border shadow-lg border-gdscGrey-200"
-        onSubmit={handleClick}
+        onSubmit={handleSubmit(handleClickDesktop)}
       >
         <div className="w-[260px] h-12 absolute left-8 top-6 flex-col space-y-2">
           <p className="text-base font-medium h-5">
@@ -45,21 +81,20 @@ export default function InputUrlField() {
           </p>
           <div>
             <input
-              value={longUrl}
-              onChange={handleLongUrl}
-              className="text-base font-normal text-gdscGrey-700 h-5 w-[16.25rem] border-b-1 outline-none "
+              {...register('longUrlDesktop')}
+              className="text-base font-normal text-gdscGrey-700 h-5 w-[16.25rem] border-b-1 outline-none"
               placeholder="Input the URL you want to shorten"
             />
-            {alert && <p className="text-gdscRed-300">Invalid Url!</p>}
+            <p className="text-gdscRed-300">
+              {errors.longUrlDesktop && errors.longUrlDesktop.message}
+            </p>
           </div>
         </div>
         <ReactLogo className="absolute top-12 left-[292px]" />
         {!loading ? (
           <button
             type="submit"
-            className={`absolute inset-y-5 right-5 hidden text-base text-white md:block w-[152px] h-[60px] bg-gdscBlue-300 rounded-[8px] hover:bg-shorten-btn-hover ease-out duration-300 ${
-              !longUrl && 'cursor-not-allowed'
-            }`}
+            className="absolute inset-y-5 right-5 hidden text-base text-white md:block w-[152px] h-[60px] bg-gdscBlue-300 rounded-[8px] hover:bg-shorten-btn-hover ease-out duration-300 "
           >
             Shorten
           </button>
@@ -80,20 +115,21 @@ export default function InputUrlField() {
       <div className="relative md:hidden bg-white rounded-[8px] mr-5 h-[72px] flex items-center pl-6 space-x-5 border shadow-lg border-gdscGrey-200">
         <ReactLogo />
         <input
-          value={longUrl}
-          onChange={handleLongUrl}
+          {...register('longUrlMobile')}
           className="text-base font-normal text-gdscGrey-700 h-5 w-full bg-white outline-none pr-[32px]"
           placeholder="Input the URL you want to shorten"
         />
       </div>
-      {alert && <p className="text-gdscRed-300 md:hidden">Invalid Url!</p>}
+      {errors.longUrlMobile && (
+        <p className="text-gdscRed-300 md:hidden">
+          {errors.longUrlMobile.message}
+        </p>
+      )}
       {!loading ? (
         <button
           type="button"
-          className={`text-base text-white md:hidden w-[152px] h-[60px] bg-gdscBlue-300 rounded hover:bg-shorten-btn-hover ${
-            !longUrl && 'cursor-not-allowed'
-          }`}
-          onClick={handleClick}
+          className="text-base text-white md:hidden w-[152px] h-[60px] bg-gdscBlue-300 rounded hover:bg-shorten-btn-hover "
+          onClick={handleSubmit(handleClickMobile)}
         >
           Shorten
         </button>
