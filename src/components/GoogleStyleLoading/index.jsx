1faba2429ab885c06182ec6de6161cffbfccaa36
Trần Duy Minh <minhduytranct2017@gmail.com>
2022-03-18 22:12:01 +0700

    [f] 761 - Add lottie Google style animation
    - Create Google style loading animation component
    - Add animation to homepage (not logged in)
    
    [#181283761] https://www.pivotaltracker.com/story/show/181283761

---
+++ src/components/GoogleStyleLoading/index.jsx
@@ -0,0 +1,20 @@
+import Lottie from 'lottie-react';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import googleLoading from 'assets/animation/google-loading.json';
+
+export default function GoogleLoadingAnimation({ show }) {
+  return (
+    <div
+      className={`fixed z-50 inset-0 bg-gdscGrey-200 bg-opacity-50 flex justify-center items-center ${
+        show ? 'block' : 'hidden'
+      }`}
+    >
+      <Lottie className="ml-[64px]" animationData={googleLoading} loop />
+    </div>
+  );
+}
+GoogleLoadingAnimation.propTypes = {
+  show: PropTypes.bool.isRequired,
+};
